2ea188d9720d4b9392449d1f703f10cbd47130db
Gisselissa <gisecapozzi@gmail.com>
2023-04-21 15:37:11 -0300

    Segundo ejercicio de javascript

---
+++ _javascript/ejercicio02/index.js
@@ -0,0 +1,10 @@
+let nombre = "Gisela";
+let edad = 34;
+let desarrollador = true;
+let fec_nacimiento = new Date("march 23 1989");
+let libroFav = {
+  titulo: 'Los Miserables',
+  autor: 'Victor Hugo',
+  fecha: 1862,
+  url: 'https://www.cuspide.com/9789878317601/Los+Miserables'
+};
