903d7032bc6ef7ce5ed20eaab8f95106a5535178
Gisselissa <gisecapozzi@gmail.com>
2023-04-24 16:32:53 -0300

    Tercer ejercicio de javascript

---
+++ _javascript/ejercicio03/index.js
@@ -0,0 +1,29 @@
+// Factorial for
+
+let total = 1;
+let num = 5;
+
+/* for (let i = 1; i <= num; i++) {
+  console.log(total = total * i);
+} */
+
+// Factorial while
+let j = 1;
+let num2 = 7;
+let total2 = 1;
+/* while (j <= num2) {
+  console.log(total2 = total2 * j)
+  j++
+} */
+
+// Factorial con break
+let total3 = 1;
+let num3 = 3;
+
+for (let k = 1; k <= num; k++) {
+  if (k >= 24) {
+    break;
+  }
+  total3 = total3 * k
+  console.log(total3)
+}
