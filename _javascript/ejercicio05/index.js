454dd8b32d0bf11b810bc1b3ba229af28deea528
Gisselissa <gisecapozzi@gmail.com>
2023-04-25 14:59:20 -0300

    Quinto ejercicio de javascript

---
+++ _javascript/ejercicio05/index.js
@@ -0,0 +1,7 @@
+let alturaCentimetro = 158;
+let alturaMetro = 1.58;
+let pesoKilogramos = 99.210;
+let alturaMetroArriba = Math.round(Math.ceil(alturaMetro));
+let alturaMetroAbajo = Math.round(Math.floor(alturaMetro));
+let maxValor = 'El máximo valor que se puede obtener en Javascript + 1' === Number.MAX_VALUE;
+console.log(maxValor);
