1ed1c6308cb06323b0b17168beea7e61e671adf7
Gisselissa <gisecapozzi@gmail.com>
2023-04-25 11:38:50 -0300

    Cuarto ejercicio de javascript

---
+++ _javascript/ejercicio04/index.js
@@ -0,0 +1,11 @@
+// Cadena de texto
+let nombre =  'Gisela';
+let apellido = 'Capozzi';
+let estudiante = nombre.concat(' ', apellido);
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let contarEspacios = estudiante.length;
+let primeraLetraNombre = nombre.charAt();
+let ultimaLetraApellido = apellido.charAt(apellido.length - 1);
+let eliminaEspacios = estudiante.replace(' ', '');
+let contieneNombre = estudiante.includes(nombre);
