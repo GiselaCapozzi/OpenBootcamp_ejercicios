8294ba32b463d2a6e60849929769d7304adb6a58
Gisselissa <gisecapozzi@gmail.com>
2023-04-26 15:08:36 -0300

    Septimo ejecicio javascript A

---
+++ _javascript/ejercicio07/index1.js
@@ -0,0 +1,10 @@
+const familia = ['Albina', 'Gabriela', 'Pablo', 'Alba', 'Juan', 'Gisela'];
+
+const newSet = new Set(familia);
+console.log(newSet);
+
+newSet.add('Gisela');
+console.log(newSet);
+
+newSet.add('Javascript');
+console.log(newSet);
